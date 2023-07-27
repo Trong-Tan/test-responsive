4186dc5613ba1cc7d30bab0d06d0df3339cc125f
Trong-Tan <trongtan.ttt0503@gmail.com>
2023-07-27 16:18:23 +0700

    udate file

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    // Các plugin khác nếu cần thiết
+  ],
 };
